1b21e1ca3a0c4eb4f198e42f64caa80b9d4b37af
Koushik <dask9344@gmail.com>
2025-03-08 04:48:39 +0530

    bit changes commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import Navbar from "@/components/navbar";
-import Footer from '@/components/Footer';
+import Footer from '@/components/footer';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
