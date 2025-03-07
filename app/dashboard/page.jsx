ebffb53295e164f82382074a3c0d6dbd49de72ea
Koushik <dask9344@gmail.com>
2025-03-08 05:08:54 +0530

    bit changes again commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
-import Topics from "@/components/topics";
+import Topics from '@/components/topics';
 
 export default function Dashboard() {
-  return ( 
+  return (
     <main>
       <h1>Dashboard</h1>
       <Topics />
